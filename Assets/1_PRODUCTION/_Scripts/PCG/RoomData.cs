8b9919aa24b26a1cf0bf65ce56b0792403db6809
MickDmnts <michaeldiamantis17@gmail.com>
2022-12-10 03:11:00 +0200

    Fully commented PCG System. @AlexandrosPapadakis

--- Assets/1_PRODUCTION/_Scripts/PCG/RoomData.cs
+++ Assets/1_PRODUCTION/_Scripts/PCG/RoomData.cs
@@ -3,24 +3,71 @@ using UnityEngine;
 
 namespace AKB.Core.Managing.PCG
 {
+    /// <summary>
+    /// This class is responsible for holding all the information a room will need to 
+    /// interact with the PCG system.
+    /// </summary>
     public class RoomData : MonoBehaviour
     {
+        /// <summary>
+        /// The room player spawn point.
+        /// </summary>
         [Header("Set in inspector")]
-        [SerializeField] Transform roomEntryPoint;
-        [SerializeField] List<Transform> roomSpawnPoints;
-        [SerializeField] RoomWorld correspondingWorld;
-        [SerializeField] RoomType roomType;
+        [SerializeField, Tooltip("The room player spawn point.")] Transform roomEntryPoint;
+        /// <summary>
+        /// The room enemy spawn points.
+        /// </summary>
+        [SerializeField, Tooltip("The room enemy spawn points.")] List<Transform> roomSpawnPoints;
+        /// <summary>
+        /// The world the room belongs to.
+        /// </summary>
+        [SerializeField, Tooltip("The world the room belongs to.")] RoomWorld correspondingWorld;
+        /// <summary>
+        /// The type of this room.
+        /// </summary>
+        [SerializeField, Tooltip("The type of this room.")] RoomType roomType;
 
+        #region ROOM_INIT
+        /// <summary>
+        /// THIS rooms' ID set from the RoomDataContainer initializer.
+        /// </summary>
         int roomID;
 
+        /// <summary>
+        /// Get the rooms' ID.
+        /// </summary>
+        public int GetRoomID() => roomID;
+        /// <summary>
+        /// Set THIS rooms' ID.
+        /// </summary>
+        public void SetRoomID(int value) { roomID = value; }
+        #endregion
+
+        #region EXTERNAL_USE
+        /// <summary>
+        /// Get the rooms' player spawn point.
+        /// </summary>
         public Transform GetRoomEntryPoint() => roomEntryPoint;
 
-        public int GetRoomID() => roomID;
-        public void SetRoomID(int value) { roomID = value; }
-
+        /// <summary>
+        /// Get this rooms' corresponding world type.
+        /// </summary>
         public RoomWorld GetRoomWorld() => correspondingWorld;
+
+        /// <summary>
+        /// Get this rooms' type.
+        /// </summary>
         public RoomType GetRoomType() => roomType;
+
+        /// <summary>
+        /// Get this rooms game object.
+        /// </summary>
         public GameObject GetRoomPrefab() => gameObject;
+
+        /// <summary>
+        /// Get this rooms list of spawn points.
+        /// </summary>
         public List<Transform> GetRoomSpawnPoints() => roomSpawnPoints;
+        #endregion
     }
 }
