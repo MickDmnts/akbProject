a6f70f2b254f313588ebeb257bcf878ab7ce5be1
NikosDulca <13-13245@saeinstitute.edu>
2022-12-10 17:03:15 +0200

    Persistent Advancements
    i wrote a script that update an image and a text according if you hold the button more than 2 sec

--- Assets/2_DEVELOPEMT/_Scripts/PlayerUI/PersistentAdvancements.cs
+++ Assets/2_DEVELOPEMT/_Scripts/PlayerUI/PersistentAdvancements.cs
@@ -1,18 +1,122 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
 
-public class PersistentAdvancements : MonoBehaviour
+public class PersistentAdvancements : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    bool pointerDown;
+
+    float pointerDownTimer;
+
+    float requiredHoldTime = 2f;
+
+    int currentlvl = 0;
+
+    [SerializeField] Image abilityImage;
+
+    [SerializeField] TextMeshProUGUI lvl;
+
+    //[SerializeField] UnityEvent onHold;
+
+
+    public void OnPointerDown(PointerEventData pointerEventData)
     {
-        
+        pointerDown = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    //Detect if clicks are no longer registering
+    public void OnPointerUp(PointerEventData pointerEventData)
     {
-        
+        if (currentlvl == 0)
+        {
+            Reset();
+
+            abilityImage.fillAmount = 0;
+        }
+        else if (currentlvl == 1)
+        {
+            Reset();
+
+            abilityImage.fillAmount = 0.33f;
+        }
+        else if(currentlvl == 2)
+        {
+            Reset();
+
+            abilityImage.fillAmount = 0.66f;
+        }
+    }
+
+    private void Update()
+    {
+        if (pointerDown)
+        {
+            pointerDownTimer += Time.deltaTime;
+
+            if (pointerDownTimer > requiredHoldTime)
+            {
+                //if (onHold != null)
+                //{
+                //    onHold.Invoke();
+                //}
+
+                if (currentlvl == 0)
+                {
+                    currentlvl++;
+                    lvl.text = currentlvl + " / " + 3;
+
+                    Reset();
+                }
+                else if(currentlvl == 1)
+                {
+                    currentlvl++;
+                    lvl.text = currentlvl + " / " + 3;
+
+                    Reset();
+                }
+                else if (currentlvl == 2)
+                {
+                    currentlvl++;
+                    lvl.text = currentlvl + " / " + 3;
+
+                    Reset();
+                }
+            }
+
+            if (currentlvl == 0)
+            {
+                //The interpolation value between the two floats.
+                float t = pointerDownTimer / requiredHoldTime;
+                abilityImage.fillAmount = Mathf.Lerp(0, 0.33f, t);
+            }
+            if (currentlvl == 1)
+            {
+                //The interpolation value between the two floats.
+                float t = pointerDownTimer / requiredHoldTime;
+                abilityImage.fillAmount = Mathf.Lerp(0.33f, 0.66f, t);
+            }
+            if (currentlvl == 2)
+            {
+                //The interpolation value between the two floats.
+                float t = pointerDownTimer / requiredHoldTime;
+                abilityImage.fillAmount = Mathf.Lerp(0.66f, 1, t);
+            }
+        }
+    }
+
+    public void Testing()
+    {
+        Debug.Log("testing");
+    }
+
+
+    private void Reset()
+    {
+        pointerDown = false;
+        pointerDownTimer = 0;
     }
 }
