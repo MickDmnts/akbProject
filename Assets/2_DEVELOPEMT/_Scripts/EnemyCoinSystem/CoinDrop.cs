d6d7a7591f3a15c81f6fb525ba86579f0e2571d3
maritinakltr <13-13301@saeinstitute.edu>
2022-12-12 14:21:14 +0200

    CoinDrop(Not Finished).
    Need to create a new script with a state that checks if a previous enemy is dead.

--- Assets/2_DEVELOPEMT/_Scripts/EnemyCoinSystem/CoinDrop.cs
+++ Assets/2_DEVELOPEMT/_Scripts/EnemyCoinSystem/CoinDrop.cs
@@ -4,19 +4,43 @@ using UnityEngine;
 
 public class CoinDrop : MonoBehaviour
 {
-    public GameObject Coin;
-    public float health = 100f;
-    public Transform coinDropPos;
+    [SerializedFieled] public GameObject Coin;
+    [SerializedFieled] public float health = 100f;
+    [SerializedFieled] public Transform coinDropPos;
+    [SerializedFieled] public float coinNumber;
 
-    //For ASSIGNES in this section
-    // Coin dropage should not be handled here, instead just generate a multiplyer number that multiplies the 
-    // initial coin drop value of an enemy. Hence the structure should be:
-    //Player attacks -> multiplyer increases by a small amount (inspector mutated)
-    //Player gets hit OR a timer (inspector mutated) -> multiplyer back to 1f;
-    //REPEAT 
-    /* void DropCoin()
+    public void EnemyDies()
     {
-        Vector3 position = coinDropPos.position;
-        GameObject coin = Instantiate(Coin, position, Quaternion.identity);
-    } */
+        if(health <=0f)
+        {
+            public void Die()
+            {
+                Destroy(gameObject);
+                DropCoin();
+            }
+        }
+    }
+    //(TO DO: In a new script create a state that checks if a previous enemy is dead. If the state is true then coinNumber++.)
+    //(+ a timer for it.)
+    //public void CoinCount()  
+    //{
+    //    if()
+    //    {
+
+    //    }
+    //    else
+    //    {
+    //        coinNumber = 25f;
+    //    }
+    //}
+
+    void DropCoin()
+    {
+        CoinCount();
+        Vector3 position = transform.position;
+        GameObject coin = Instantiate(Coin, position + new Vector3(0.0f, 1.0f, 0.0f), Quaternion.identity);
+        coin.SetActive(true);
+    }
+
+
 }
