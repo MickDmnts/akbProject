1fed75b2baec82f55fb246eb03bb179cf69f45b0
MickDmnts <michaeldiamantis17@gmail.com>
2022-12-14 18:11:36 +0200

    Audio handler script
    Created audio handler script and added comments of what is to come.

---
+++ Assets/1_PRODUCTION/_Scripts/_Managing/_Core/SoundsHandler.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace AKB.Core.Sounds
+{
+    public class SoundsHandler : MonoBehaviour
+    {
+        /* TODO
+        * Make a list with every audio clip of the game.
+        * Make audio source handlers, mutators and accessors
+        * Separate audio source for music
+        * Separate audio source for sfx
+        * Use the game master audio for master audio slider
+        */
+    }
+}
