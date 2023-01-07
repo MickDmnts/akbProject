052cbc9117a436a94a3a4580b3e47d02711cd1cf
MickDmnts <michaeldiamantis17@gmail.com>
2023-01-07 16:32:28 +0200

    Base astaroth script.

--- Assets/2_DEVELOPEMT/_Scripts/Boss_Astaroth/BossAstaroth.cs
+++ Assets/2_DEVELOPEMT/_Scripts/Boss_Astaroth/BossAstaroth.cs
@@ -6,8 +6,21 @@ using akb.Entities.Interactions;
 
 namespace akb.Entities.AI.Implementations.Astaroth
 {
+    public enum AstarothPhases
+    {
+        Phase1,
+        Phase2,
+        Phase3
+    }
+
     public class BossAstaroth : AI_Entity, IInteractable
     {
+        [SerializeField] GameObject projectilePrefab;
+        [SerializeField] ParticleSystem flamePillarPs;
+        [SerializeField] float phase1ProjectileRps = 3f;
+
+        Transform target;
+
         public void AttackInteraction(float damageValue)
         {
             throw new System.NotImplementedException();
