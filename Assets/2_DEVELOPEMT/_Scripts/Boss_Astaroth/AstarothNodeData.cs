e4e099356f8131e89f4999f06be0ba35b3864dab
DimitrisGkr <92577612+DimitrisGkr@users.noreply.github.com>
2023-01-07 16:54:51 +0200

    NodeDataPush

--- Assets/2_DEVELOPEMT/_Scripts/Boss_Astaroth/AstarothNodeData.cs
+++ Assets/2_DEVELOPEMT/_Scripts/Boss_Astaroth/AstarothNodeData.cs
@@ -2,6 +2,9 @@ namespace akb.Entities.AI.Implementations.Astaroth
 {
     public class AstarothNodeData : AI_NodeData
     {
-
+        public void SetEnemyEntity(BossAstaroth value) => ai_entity = value;
+        public void SetEnemyAnimations(BossAstarothAnimations value) => ai_animations = value;
+        public BossAstaroth GetEnemyEntity() => ai_entity as BossAstaroth;
+        public BossAstarothAnimations GetDemonAnimations() => ai_animations as BossAstarothAnimations
     }
 }
