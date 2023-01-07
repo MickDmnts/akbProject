415c695d5e0f4faa986e13011b163aa1b2929241
MickDmnts <michaeldiamantis17@gmail.com>
2023-01-07 16:55:57 +0200

    Update AstarothNodeData.cs

--- Assets/2_DEVELOPEMT/_Scripts/Boss_Astaroth/AstarothNodeData.cs
+++ Assets/2_DEVELOPEMT/_Scripts/Boss_Astaroth/AstarothNodeData.cs
@@ -2,9 +2,14 @@ namespace akb.Entities.AI.Implementations.Astaroth
 {
     public class AstarothNodeData : AI_NodeData
     {
+        #region MUTATORS
         public void SetEnemyEntity(BossAstaroth value) => ai_entity = value;
         public void SetEnemyAnimations(BossAstarothAnimations value) => ai_animations = value;
+        #endregion
+
+        #region ACCESSORS
         public BossAstaroth GetEnemyEntity() => ai_entity as BossAstaroth;
-        public BossAstarothAnimations GetDemonAnimations() => ai_animations as BossAstarothAnimations
+        public BossAstarothAnimations GetDemonAnimations() => ai_animations as BossAstarothAnimations;
+        #endregion
     }
 }
