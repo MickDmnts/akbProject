aa97d44bf71cdcfb60c8a7695cd45e4179f6e7cd
MickDmnts <michaeldiamantis17@gmail.com>
2023-01-22 22:08:16 +0200

    Created a sound manager that @AlexandrosPapadakis can use to play sounds.

--- Assets/1_PRODUCTION/_Scripts/_Managing/_Core/SoundsHandler.cs
+++ Assets/1_PRODUCTION/_Scripts/_Managing/_Core/SoundsHandler.cs
@@ -1,15 +1,93 @@
+using System.Collections.Generic;
+using akb.Core.Managing;
+using akb.Core.Managing.LevelLoading;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace akb.Core.Sounds
 {
+    public enum GameAudioClip
+    {
+        //Populate with sfx names
+    }
+
     public class SoundsHandler : MonoBehaviour
     {
-        /* TODO
-        * Make a list with every audio clip of the game.
-        * Make audio source handlers, mutators and accessors
-        * Separate audio source for music
-        * Separate audio source for sfx
-        * Use the game master audio for master audio slider
-        */
+        [Header("Set in inspector")]
+        [SerializeField] List<AudioClip> levelWideClips;
+        [SerializeField] List<AudioClip> sfxClips;
+
+        public AudioSource mainAudioSource;
+        public AudioSource sfxSource;
+
+        Slider musicSlider;
+
+        private void Start()
+        {
+            ManagerHUB.GetManager.GameEventsHandler.onSceneChanged += ChangeSoundtrack;
+        }
+
+        void ChangeSoundtrack(GameScenes scenes)
+        {
+            switch (scenes)
+            {
+                case GameScenes.PlayerScene: //What plays in the main menu
+                    //mainAudioSource.clip = levelWideClips[0];
+                    //mainAudioSource.Play();
+                    break;
+
+                case GameScenes.PlayerHUB: //What plays in the hub
+                    //mainAudioSource.clip = levelWideClips[1];
+                    //mainAudioSource.Play();
+                    break;
+
+                case GameScenes.World1Scene: //What plays in world 1
+                    //mainAudioSource.clip = levelWideClips[2];
+                    //mainAudioSource.Play();
+                    break;
+
+                case GameScenes.World2Scene: //What plays in world 2
+                    //mainAudioSource.clip = levelWideClips[3];
+                    //mainAudioSource.Play();
+                    break;
+            }
+        }
+
+        public void SetSoundReferences(Slider musicSlider)
+        {
+            this.musicSlider = musicSlider;
+        }
+
+        public void SetVolume(float value)
+        {
+            mainAudioSource.volume = value;
+            sfxSource.volume = value;
+
+            musicSlider.value = value * 10f;
+        }
+
+        ///<summary>Plays the passed clip one time</summary>
+        public void PlayOneShot(GameAudioClip clip)
+        {
+            sfxSource.PlayOneShot(sfxClips[(int)clip]);
+        }
+
+        ///<summary>Mutes/unmutes the sound</summary>
+        public void SetGameWideSoundtrackState(bool pause)
+        {
+            if (pause)
+            {
+                mainAudioSource.Pause();
+            }
+            else
+            {
+                mainAudioSource.UnPause();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            ManagerHUB.GetManager.GameEventsHandler.onSceneChanged -= ChangeSoundtrack;
+        }
     }
 }
