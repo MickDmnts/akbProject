49e4c93925b1957fdaf46205d8ce9bb73510919f
MickDmnts <michaeldiamantis17@gmail.com>
2023-02-17 00:49:14 +0200

    Fixed trap animation event.

--- Assets/1_PRODUCTION/_Scripts/Traps/SpikeAnimationScript.cs
+++ Assets/1_PRODUCTION/_Scripts/Traps/SpikeAnimationScript.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace akb.Entities.Interactions
@@ -8,55 +6,44 @@ namespace akb.Entities.Interactions
     {
         [SerializeField] float spikeHitRate = 2f;
         [SerializeField] float damageValue = 10f;
+
         float nextSpikeHit;
         Animator animator;
         bool played = false;
+
         void Start()
         {
             animator = GetComponent<Animator>();
             nextSpikeHit = Time.time + spikeHitRate;
         }
-        //private void OnTriggerEnter(Collider other)
-        //{
-        //    if (Time.time > nextSpikeHit)
-        //    {
-        //        if (other.gameObject.CompareTag("Player"))
-        //        {
-        //            animator.SetTrigger("Up");
-        //            played = true;
-        //        }
-        //    }
-        //}
-        private void OnTriggerStay(Collider other)
+
+        private void OnTriggerEnter(Collider other)
         {
-            played = false;
-            animator.SetTrigger("Down");
             if (Time.time > nextSpikeHit)
             {
                 if (other.gameObject.CompareTag("Player"))
                 {
                     animator.SetTrigger("Up");
-                    nextSpikeHit = Time.time + spikeHitRate;
                     played = true;
-                    if (played == true)
-                    {
-                        other.TryGetComponent<IInteractable>(out IInteractable interactable);
-                        interactable.AttackInteraction(damageValue);
-                    }
                 }
             }
         }
-        //public void TrapDamage()
-        //{
-        //    Collider[] colliders = Physics.OverlapSphere(transform.position,2);
-        //    for(int i = 0; i< colliders.Length; i++)
-        //    {
-        //        if(colliders[i].TryGetComponent<IInteractable>(out IInteractable interactable))
-        //        {
-        //            interactable.AttackInteraction(damageValue);
-        //        }
-        //    }
-        //}
+
+        public void TrapDamage()
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, 2);
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                if (colliders[i] == null) continue;
+                Debug.Log(colliders[i].name);
+
+                if (colliders[i].TryGetComponent<IInteractable>(out IInteractable interactable))
+                {
+                    interactable.AttackInteraction(damageValue);
+                }
+            }
+        }
+
         private void OnTriggerExit(Collider other)
         {
             if (!played) return;
