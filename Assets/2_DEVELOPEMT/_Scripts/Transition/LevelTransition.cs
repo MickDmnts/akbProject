c3ee786bfe2bcd7dc7c5ed765bf4de773546839a
MickDmnts <michaeldiamantis17@gmail.com>
2023-02-19 05:13:26 +0200

    Update LevelTransition.cs

--- Assets/2_DEVELOPEMT/_Scripts/Transition/LevelTransition.cs
+++ Assets/2_DEVELOPEMT/_Scripts/Transition/LevelTransition.cs
@@ -3,28 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class LevelTransition : MonoBehaviour
 {
-    public Animator animator;
-    private int levelToLoad;
-    // Update is called once per frame
+    Animator animator;
+
     private void Start()
     {
         animator = GetComponent<Animator>();
     }
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha0))
-        {
-            FadeOut();
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha9))
-        {
-            FadeIn();
-        }
-    }
+
     public void FadeOut()
     {
         animator.SetTrigger("FadeOut");
     }
+
     public void FadeIn()
     {
         animator.SetTrigger("FadeIn");
